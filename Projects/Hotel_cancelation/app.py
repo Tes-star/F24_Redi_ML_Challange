b0e3a12581877a5456a8f9676f593c919b5ce8d7
Timo van den Wyenbergh <vdw.timo@googlemail.com>
2024-11-28 21:56:43 +0100

    c

--- Projects/Hotel_cancelation/app.py
+++ Projects/Hotel_cancelation/app.py
@@ -64,7 +64,7 @@ def detect_delimiter(uploaded_file) -> str:
     return None
 
 # Display title and instructions
-st.title("Project Hotel cancelation 🏨: \n Score Evaluation ")
+st.title("Score Evaluation: Project Hotel cancelation 🏨")
 st.write("""Upload your predition CSV with two columns: 'id', 'is_canceled'. Ensure the file includes all required IDs, without any missing IDs.""")
 st.write("A sample file named sample_submission.csv was provided for guidance.")
 st.write("### Leaderboard")
